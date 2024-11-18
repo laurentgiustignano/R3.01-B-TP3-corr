c8eafde9cf365c23a9471d855f528292adb04039
Laurent Giustignano <laurent.giustignano@free.fr>
2024-11-18 17:16:47 +0100

    exo2

--- Glace.php
+++ Glace.php
@@ -13,33 +13,16 @@ class Glace {
     $this->addition = 0;
   }
 
-  public function ajoutParfum ($parfum) : void {
-    $this->addition += $this->getTarifParfum ($parfum);
-  }
-
-  public function getTarifParfum (string $parfum = null) : int {
-    if($parfum === "Vanille")
-      return $this->tarifParfum + 1;
-    return $this->tarifParfum;
-  }
-
-  public function getTarifTopping () : int {
-    return $this->tarifTopping;
-  }
-
-  public function getTarifCornet () : float {
+  public function getTarifCornet() : float {
     return $this->tarifCornet;
   }
 
-  public function ajoutTopping () : void {
-    $this->addition += $this->getTarifTopping ();
+  public function getTarifTopping() : int {
+    return $this->tarifTopping;
   }
 
-  public function ajoutCornet () : void {
-    $this->addition += $this->getTarifCornet ();
+  public function getTarifParfum () : int {
+    return $this->tarifParfum;
   }
 
-  public function getAddition () : float {
-    return $this->addition;
-  }
 }
--- glacier.php
+++ glacier.php
@@ -1,7 +1,13 @@
 <?php
 
+require_once 'Glace.php';
 require_once 'devtools.php';
 require_once 'header.php';
+
+$glace = new Glace();
+debug($glace->getTarifParfum());
+debug($glace->getTarifCornet());
+debug($glace->getTarifTopping());
 
 debug($_POST);
 
